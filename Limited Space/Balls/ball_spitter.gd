55dbb2b9b00d093475fe5beb6b0c63d167ddd860
Bosco <bosco.publicvoid@gmail.com>
2023-09-30 23:15:49 +0200

    added randomization to "attacks"

--- Limited Space/Balls/ball_spitter.gd
+++ Limited Space/Balls/ball_spitter.gd
@@ -3,14 +3,16 @@ extends Node3D
 
 @onready var golfer = $Golfer_Sprite
 var ballGeneric = preload("res://Balls/basic_ball.tscn")
-#var ballGeneric = preload("res://Balls/scatter_ball.tscn")
+var ballScatter = preload("res://Balls/scatter_ball.tscn")
+
+var ballarray = [ballGeneric, ballScatter]
 
 func emit():
 	#try and work out a rough speed to shoot towards the player
 	var new_speed = position.distance_to(get_tree().get_first_node_in_group("player").position)
 	
 	#create new ball
-	var newBall = ballGeneric.instantiate()
+	var newBall = ballarray.pick_random().instantiate()
 	add_sibling(newBall)
 	newBall.apply_new_force(get_global_transform().basis.z, new_speed)
 	newBall.position = position
