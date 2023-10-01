4d6fbe18bb1cab1c870c4f2f3e764e810cd3f95c
Bosco <bosco.publicvoid@gmail.com>
2023-10-01 14:53:37 +0200

    golfers generate

--- Limited Space/Balls/ball_spitter.gd
+++ Limited Space/Balls/ball_spitter.gd
@@ -1,29 +1,28 @@
 extends Node3D
 
+@export var rate_of_fire = 4
+
+@export var balltype : PackedScene
 
 @onready var golfer : AnimatedSprite3D = $Golfer_Sprite
 @onready var timer : Timer = $Shot_Timer
 
 @onready var audioplayer = $AudioStreamPlayer3D
 
-var ballGeneric = preload("res://Balls/basic_ball.tscn")
-var ballScatter = preload("res://Balls/scatter_ball.tscn")
-var ballTomato = preload("res://Balls/tomato.tscn")
 
-var ballarray = [ballGeneric, ballScatter, ballTomato]
 
 func _ready():
 	var root = get_tree().get_first_node_in_group("LevelRoot")
 	
 	if root != null && root.has_signal("LevelEnded") : 
 		root.LevelEnded.connect(_on_level_ended)
-
+	
 func emit():
 	#try and work out a rough speed to shoot towards the player
 	var new_speed = position.distance_to(get_tree().get_first_node_in_group("player").position)
 	
 	#create new ball
-	var newBall = ballarray.pick_random().instantiate()
+	var newBall = balltype.instantiate()
 	get_tree().root.add_child(newBall)
 	newBall.apply_new_force(get_global_transform().basis.z, new_speed)
 	newBall.position = position
@@ -32,7 +31,7 @@ func _on_shot_timer_timeout():
 	look_at(get_tree().get_first_node_in_group("player").position)
 	rotate_y(3.1415)
 	golfer.play("default")
-	$Shot_Timer.start(randi()%5)
+	$Shot_Timer.start(rate_of_fire * randf_range(0.8, 1.2))
 
 func _on_golfer_sprite_frame_changed():
 	if golfer.frame == 1:
--- Limited Space/scripts/Golfer Manager.gd
+++ Limited Space/scripts/Golfer Manager.gd
@@ -4,23 +4,37 @@ extends Node
 
 @onready var player = get_tree().get_first_node_in_group("player")
 
-var golfer_generic = preload("res://Balls/ball_spitter.tscn")
+var golfer_generic = preload("res://Balls/Golfers/generic_golfer.tscn")
+var golfer_scatter = preload("res://Balls/Golfers/scatter_golfer.tscn")
+var golfer_tomato = preload("res://Balls/Golfers/tomato_golfer.tscn")
 
 var distance_from_player = 25
 
-var golfer_array = []
+var current_golfer_array = []
 
 func _ready():
 	_on_new_golfer_timeout()
 
 func _on_new_golfer_timeout():
-	if golfer_array.size() < Global.MAX_GOLFERS:
-		pass
-		golfer_array.pop_front()
+	if current_golfer_array.size() < Global.MAX_GOLFERS:
+		current_golfer_array.pop_front()
 		
-	var newGolfer = golfer_generic.instantiate()
+	var newGolfer = get_golfer().instantiate()
 	add_child(newGolfer)
 	var direction = Vector2.UP.rotated(randf_range(0, 2*PI))* distance_from_player * randf_range(0.7, 1.4)
 	var direction3 = Vector3(direction.x, 2, direction.y)
 	newGolfer.global_position = player.global_position + (direction3)
-	golfer_array.append(newGolfer)
+	current_golfer_array.append(newGolfer)
+
+
+func get_golfer():
+	var currentscore =  Global.PLAYER_SCORE[0]
+	
+	var code = randi() % (currentscore + 1)
+	
+	if code < 10:
+		return golfer_generic
+	if code < 50:
+		return golfer_scatter
+	else:
+		return golfer_tomato
