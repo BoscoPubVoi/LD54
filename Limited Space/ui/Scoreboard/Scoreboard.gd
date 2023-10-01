a192acbc65a50052e114a0af703c1ce48cd62e79
Bosco <bosco.publicvoid@gmail.com>
2023-10-01 22:03:43 +0200

    fix bug

--- Limited Space/ui/Scoreboard/Scoreboard.gd
+++ Limited Space/ui/Scoreboard/Scoreboard.gd
@@ -17,6 +17,6 @@ func _on_main_menu_pressed():
 func _ready():
 	TotalScoreLabel.text = str(Global.GetTotalScore())
 #	ScoreLabel.text = str(Global.PLAYER_SCORE[Global.CURRENT_LEVEL - 1], "x", " BOGEY")
-	ScoreLabel.text = "[center]" + Global.bogey_calculator(Global.PLAYER_SCORE[Global.CURRENT_LEVEL - 1]) + "[/center]"
+	ScoreLabel.text = "[center]" + Global.fun_bogey_calculator(Global.PLAYER_SCORE[Global.CURRENT_LEVEL - 1]) + "[/center]"
 
 	AudioManager.play("applause")
