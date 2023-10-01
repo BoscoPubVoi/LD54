80bf1e2ccf7acc93eac3d1d3d87fa1b181110f47
Bosco <bosco.publicvoid@gmail.com>
2023-10-01 18:56:46 +0200

    changed movement, reduced floatiness

--- Limited Space/Balls/basic_ball.gd
+++ Limited Space/Balls/basic_ball.gd
@@ -44,6 +44,10 @@ func _physics_process(_delta):
 		$ShadowMesh.set_surface_override_material(0, mat)
 	else:
 		shadow.hide()
+	if linear_velocity.y < 0:
+		gravity_scale = 2
+	else:
+		gravity_scale = 1
 
 
 func _on_body_entered(body:Node):
