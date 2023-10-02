ab34a36941e17b00135613c4fc9ffb083d644e7e
Hjalte Tagmose <HjalteTagmose@users.noreply.github.com>
2023-10-02 22:50:45 +0200

    throw tomato every 8

--- Limited Space/scripts/AudienceManager.gd
+++ Limited Space/scripts/AudienceManager.gd
@@ -5,17 +5,18 @@ var audience = []
 
 func _ready():
 	var scene_root = get_tree().get_first_node_in_group("LevelRoot")
-	
 	audience = get_tree().get_nodes_in_group("audience")
-	if scene_root != null && scene_root.has_signal("LevelEnded") : 
-		scene_root.LevelEnded.connect(_on_level_ended)
 
-	timer.start(1)#13
-
-func _on_timer_timeout():
+func throw_tomato():
 	print("throw tomato")
 	audience.pick_random().throw_tomato()
-	timer.start(1)#7
 
-func _on_level_ended():
-	timer.stop()
+#ugly shit, every 10 score
+var oldScore = 0
+func _process(delta):
+	var score = Global.GetTotalScore();
+	if score == oldScore:
+		return
+	if score % 10 == 0:
+		throw_tomato()
+	oldScore = score
