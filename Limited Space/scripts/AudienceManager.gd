0c86f50275d8e911d58d9fcae5f72b58508a76c1
Hjalte Tagmose <HjalteTagmose@users.noreply.github.com>
2023-10-02 22:37:56 +0200

    working audiencemanager

--- Limited Space/scripts/AudienceManager.gd
+++ Limited Space/scripts/AudienceManager.gd
@@ -1,14 +1,21 @@
 extends Node
 
 var audience = []
+@onready var timer : Timer = $Timer
 
 func _ready():
+	var scene_root = get_tree().get_first_node_in_group("LevelRoot")
+	
 	audience = get_tree().get_nodes_in_group("audience")
+	if scene_root != null && scene_root.has_signal("LevelEnded") : 
+		scene_root.LevelEnded.connect(_on_level_ended)
 
-func _process(delta):
-	if Input.is_action_just_pressed("ui_accept"):
-		print("asdasda")
-		audience.pick_random().throw_tomato()
+	timer.start(1)#13
 
-func throw_tomato():
-	pass
+func _on_timer_timeout():
+	print("throw tomato")
+	audience.pick_random().throw_tomato()
+	timer.start(1)#7
+
+func _on_level_ended():
+	timer.stop()
