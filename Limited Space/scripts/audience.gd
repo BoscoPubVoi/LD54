16a9ad27750a9242b2635d3affa2557fc7e8d16e
Hjalte Tagmose <HjalteTagmose@users.noreply.github.com>
2023-10-02 20:40:27 +0200

    add animation

--- Limited Space/scripts/audience.gd
+++ Limited Space/scripts/audience.gd
@@ -1,13 +1,13 @@
 extends Node3D
 
 @export var colors : Array[Color]
-
 @onready var sprites : Array[Sprite3D] = [
 	$torso, 
 	$legs,
 	$torso/head, 
 	$torso/arm_l, 
 	$torso/arm_r]
+@onready var anim : AnimationPlayer = $AnimationPlayer
 
 func _ready():
 	var torso = sprites[0]
@@ -16,4 +16,7 @@ func _ready():
 	torso.modulate = color
 	legs.modulate = color
 	scale = Vector3( randf_range(0.9, 1.1), randf_range(0.9,1.1), 1 )
+	
+	await get_tree().create_timer(randf_range(0, 1.0)).timeout
+	anim.play("test")
 
